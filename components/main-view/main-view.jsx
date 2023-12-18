42cb507288640e00d874574479451a7830ded0ad
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-12-18 12:31:54 +0200

    added components-mainview-moviecard-movieview folders+jsx files and contents for react movie cards, updated index.jsx to match mainview

---
+++ components/main-view/main-view.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "Movie  1",
+      genre: "Thriller",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+      director: "Marijn Haverbeke",
+    },
+    {
+      id: 2,
+      title: "Movie  2",
+      genre: "Comedy",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      director: "Federico Kereki",
+    },
+    {
+      id: 3,
+      title: "Movie  3",
+      genre: "Fantasy",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+        director: "Douglas Crockford",
+    }
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+  return (
+    <div>
+      {movies.map((book) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={() => {
+            setSelectedMovie(movie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
