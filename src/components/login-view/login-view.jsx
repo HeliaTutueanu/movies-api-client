5af5153121183a9caaaedd5cd376454c43ed4e43
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-12-22 13:03:54 +0200

    added login+signup view to components+mainview

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+    fetch("https://openlibrary.org/account/login.json", {
+        method: "POST",
+        body: JSON.stringify(data)
+      }).then((response) => {
+        if (response.ok) {
+          onLoggedIn(username);
+        } else {
+          alert("Login failed");
+        }
+      });
+
+  const [username, setUsername] = useState("");
+    <input
+      type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+    />
+
+  const [password, setPassword] = useState("");
+    <input
+      type="text"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
+
+  const handleSubmit = (event) => {
+    event.preventDefault();   // prevents the default behavior of the form which is to reload the entire page
+
+  const data = {
+    Username: username,
+    Password: password
+  };
+
+  fetch("https://openlibrary.org/account/login.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Login response: ", data);
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert("No such user");
+      }
+    })
+    .catch((e) => {
+      alert("Something went wrong");
+    });
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <label>
+      Username:
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+    </label>
+    <label>
+      Password:
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+  );
+}};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 const SimilarMovies = ({ selectedMovie, movies, onMovieClick }) => {
     if (!selectedMovie) {
@@ -31,25 +33,38 @@ const SimilarMovies = ({ selectedMovie, movies, onMovieClick }) => {
   };
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:1234/movies")
+    if (!token) return;
+ 
+    fetch("http://localhost:1234/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
-      .then((data) => {
-        const mvDB = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            title: doc.title,
-            image: "",
-            director: doc.director_name?.[0],
-            genre: doc.genre
-          };
-        });
-        setMovies(mvDB);
-       });
-  }, []);
+      .then((movies) => {
+        setMovies(movies);
+ 
+      });
+  }, [token]);
+  
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -77,6 +92,7 @@ export const MainView = () => {
 
   return (
     <div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
